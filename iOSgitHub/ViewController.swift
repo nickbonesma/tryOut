8da43df4e6e5a6a329c92667055c3220f37093c0
nickbonesma <Nick@wlan253-6.extern.hu-berlin.de>
2015-10-16 13:10:14 +0200

    Come ooonnnn

--- iOSgitHub/ViewController.swift
+++ iOSgitHub/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("let's push this to our GitHub!")
+        
+        print("you better start functioning well")
+        
     }
 
     override func didReceiveMemoryWarning() {
